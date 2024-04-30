7257efdd4fbf7da44c648fc2a3a7fee0129822de
RicardoAzevedo <ricardoazevedo1911@gmail.com>
2024-04-30 16:13:13 -0300

    Exemplo de um escopo de um método

--- src/main/java/edu/ricardooazevedo/java/revisaometodos/RevisaoMetodos.java
+++ src/main/java/edu/ricardooazevedo/java/revisaometodos/RevisaoMetodos.java
@@ -5,9 +5,29 @@ package edu.ricardooazevedo.java.revisaometodos;
  *
  * @author ricar
  */
+
+//Exemplos de métodos:
 public class RevisaoMetodos {
 
     public static void main(String[] args) {
         
     }
+    
+    /*
+      Encapsulamento (public/protected/private)
+       |   Tipo que vai retornar (int/float/double/String/boolean...)
+       |    |   Nome do método
+       |    |    |         Parametros do metodo e suas variaveis(Existem metodos que não necessitam de parametros para seu funvionamento)   
+       |    |    |                      |
+       |    |    |                      |
+       V    V    V  |-------------------------------------|
+    public int somar(int primeiroNumero, int segundoNumero){
+
+        //A funcionalidade do método
+    
+        return ... (oque o metodo vai retornar/ se for void, não precisa ter o return!)
+    }
+    */
+    
+    
 }
